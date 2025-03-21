6653c9ff5a6850c202edf0dd7b921e43a9c65c95
NjabuloJ <fanajb65@gmail.com>
2025-03-21 20:30:22 +0200

    Create follow.js

---
+++ commandes/follow.js
@@ -0,0 +1,218 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  'value': true
+});
+const {
+  zokou
+} = require("../framework/zokou");
+zokou({
+  'nomCom': "linkgroup",
+  'reaction': '🪰',
+  'categorie': "Njabulo-Jb",
+  'nomFichier': __filename
+}, async (_0x3258e7, _0x4c4732, _0x13b70c) => {
+  console.log("Commande saisie !!!s");
+  await _0x4c4732.sendMessage(_0x3258e7, {
+    'text': "Hello 👋\n\nclíck σn thє вuttσn вєlσw tσ jσín thє σffícíαl *njαвulσ jв* whαtsαpp grσup",
+    'contextInfo': {
+      'externalAdReply': {
+        'sourceUrl': "https://whatsapp.com/channel/0029VarYP5iAInPtfQ8fRb2T",
+        'mediaType': 0x1,
+        'mediaUrl': "https://files.catbox.moe/hlazmb.jpg",
+        'title': "Join Our WhatsApp Group",
+        'body': "clíck tσ jσín thє σffícíαl hαchєrs hσσd whαtsαpp grσup!"
+      }
+    }
+  });
+  console.log("Command executed: wagroup");
+});
+zokou({
+  'nomCom': 'channells',
+  'reaction': '🪰',
+  'categorie': "Njabulo-Jb",
+  'nomFichier': __filename
+}, async (_0x14c950, _0x346e6b, _0x31cbea) => {
+  console.log("Commande saisie !!!s");
+  await _0x346e6b.sendMessage(_0x14c950, {
+    'text': "Hello 👋\n\nclíck σn thє вuttσn вєlσw tσ fσllσw thє σffícíαl *hαchєrs hσσd* whαtsαpp chαnnєl",
+    'contextInfo': {
+      'externalAdReply': {
+        'https://whatsapp.com/channel/0029VarYP5iAInPtfQ8fRb2T',
+        'mediaType': 0x1,
+        'mediaUrl': "https://files.catbox.moe/hlazmb.jpg",
+        'title': "Join Our WhatsApp Channel",
+        'body': "clíck tσ jσín thє σffícíαl hαchєrs hσσd whαtsαpp chαnnєl!"
+      }
+    }
+  });
+  console.log("Command executed: wachannel");
+});
+zokou({
+  'nomCom': 'happinessxmd',
+  'reaction': '🪰',
+  'categorie': "Njabulo-Jb",
+  'nomFichier': __filename
+}, async (_0x14c950, _0x346e6b, _0x31cbea) => {
+  console.log("Commande saisie !!!s");
+  await _0x346e6b.sendMessage(_0x14c950, {
+    'text': "Hello 👋\n\nclíck σn thє вuttσn вєlσw tσ cσntαct thє σffícíαl *njαвulσ jв* σwnєr",
+    'contextInfo': {
+      'externalAdReply': {
+        'sourceUrl': 'https://wa.me/26777821911',
+        'mediaType': 0x1,
+        'mediaUrl': "https://files.catbox.moe/hlazmb.jpg",
+        'title': "Join Our Developer Place",
+        'body': "clíck tσ jσín thє σffícíαl njαвulσ jв σwnєr ínвσх!"
+      }
+    }
+  });
+  console.log("Command executed: waowner");
+});
+zokou({
+  'nomCom': 'fb-page',
+  'reaction': '🪰',
+  'categorie': "Njabulo-Jb",
+  'nomFichier': __filename
+}, async (_0x14c950, _0x346e6b, _0x31cbea) => {
+  console.log("Commande saisie !!!s");
+  await _0x346e6b.sendMessage(_0x14c950, {
+    'text': "Hello 👋\n\nclíck σn thє phσtσ вєlσw tσ fσllσw thє σffícíαl *njαвulσ jв* fαcєвσσk pαgє",
+    'contextInfo': {
+      'externalAdReply': {
+        'sourceUrl': 'https://www.facebook.com/profile.php?id=100094314013209',
+        'mediaType': 0x1,
+        'mediaUrl': "https://files.catbox.moe/hlazmb.jpg",
+        'title': "Follow Facebook Page 📄",
+        'body': "clíck tσ jσín thє σffícíαl hαchєrs hσσd fαcєвσσk pαgє!"
+      }
+    }
+  });
+  console.log("Command executed: fb-page");
+});
+zokou({
+  'nomCom': 'insta-page',
+  'reaction': '🪰',
+  'categorie': "Njabulo-Jb",
+  'nomFichier': __filename
+}, async (_0x14c950, _0x346e6b, _0x31cbea) => {
+  console.log("Commande saisie !!!s");
+  await _0x346e6b.sendMessage(_0x14c950, {
+    'text': "Hello 👋\n\nclíck σn thє phσtσ вєlσw tσ fσllσw thє σffícíαl *njαвulσ jв* ínstαgrαm pαgє",
+    'contextInfo': {
+      'externalAdReply': {
+        'sourceUrl': 'https://www.facebook.com/profile.php?id=100094314013209',
+        'mediaType': 0x1,
+        'mediaUrl': "https://files.catbox.moe/hlazmb.jpg",
+        'title': "Follow Instagram Page 📄",
+        'body': "clíck tσ jσín thє σffícíαl hαchєrs hσσd ínstαgrαm pαgє!"
+      }
+    }
+  });
+  console.log("Command executed: insta-page");
+});
+zokou({
+  'nomCom': 'threads-page',
+  'reaction': '🪰',
+  'categorie': "Njabulo-Jb",
+  'nomFichier': __filename
+}, async (_0x14c950, _0x346e6b, _0x31cbea) => {
+  console.log("Commande saisie !!!s");
+  await _0x346e6b.sendMessage(_0x14c950, {
+    'text': "Hello 👋\n\nclíck σn thє phσtσ вєlσw tσ fσllσw thє σffícíαl *njαвulσ jв* thrєαds pαgє",
+    'contextInfo': {
+      'externalAdReply': {
+        'sourceUrl': 'https://www.facebook.com/profile.php?id=100094314013209',
+        'mediaType': 0x1,
+        'mediaUrl': "https://files.catbox.moe/hlazmb.jpg",
+        'title': "Follow Threads Page 📄",
+        'body': "clíck tσ jσín thє σffícíαl hαchєrs hσσd thrєαds pαgє!"
+      }
+    }
+  });
+  console.log("Command executed: threads-page");
+});
+zokou({
+  'nomCom': 'tiktok-page',
+  'reaction': '🪰',
+  'categorie': "Njabulo-Jb",
+  'nomFichier': __filename
+}, async (_0x14c950, _0x346e6b, _0x31cbea) => {
+  console.log("Commande saisie !!!s");
+  await _0x346e6b.sendMessage(_0x14c950, {
+    'text': "Hello 👋\n\nclíck σn thє phσtσ вєlσw tσ fσllσw thє σffícíαl *njαвulσ jв* tíktσk pαgє",
+    'contextInfo': {
+      'externalAdReply': {
+        'sourceUrl': 'tiktok.com/@njabulo.jb24',
+        'mediaType': 0x1,
+        'mediaUrl': "https://files.catbox.moe/hlazmb.jpg",
+        'title': "Follow TikTok Page 📄",
+        'вσdч': "clíck tσ jσín thє σffícíαl njαвulσ jв tíktσk pαgє!"
+      }
+    }
+  });
+  console.log("Command executed: tiktok-page");
+});
+zokou({
+  'nomCom': 'tgroup',
+  'reaction': '🪰',
+  'categorie': "Njabulo-Jb",
+  'nomFichier': __filename
+}, async (_0x14c950, _0x346e6b, _0x31cbea) => {
+  console.log("Commande saisie !!!s");
+  await _0x346e6b.sendMessage(_0x14c950, {
+    'text': "Hello 👋\n\nclíck σn thє phσtσ вєlσw tσ jσín thє σffícíαl *njαвulσ jв* tєlєgrαm grσup",
+    'contextInfo': {
+      'externalAdReply': {
+        'sourceUrl': 'https://whatsapp.com/channel/0029VarYP5iAInPtfQ8fRb2T',
+        'mediaType': 0x1,
+        'mediaUrl': "https://files.catbox.moe/hlazmb.jpg",
+        'title': "Join Telegram Group📄",
+        'body': "clíck tσ jσín thє σffícíαl njαвulσ jв tєlєgrαm grσup!"
+      }
+    }
+  });
+  console.log("Command executed: tgroup");
+});
+zokou({
+  'nomCom': 'ytchannel',
+  'reaction': '🪰',
+  'categorie': "Njabulo-Jb",
+  'nomFichier': __filename
+}, async (_0x14c950, _0x346e6b, _0x31cbea) => {
+  console.log("Commande saisie !!!s");
+  await _0x346e6b.sendMessage(_0x14c950, {
+    'text': "Hello 👋\n\nclíck σn thє phσtσ вєlσw tσ suвscríвє thє σffícíαl *njαвulσ jв* чσutuвє chαnnєl",
+    'contextInfo': {
+      'externalAdReply': {
+        'sourceUrl': 'https://www.youtube.com/@Njabulo-Jboffice',
+        'mediaType': 0x1,
+        'mediaUrl': "https://files.catbox.moe/hlazmb.jpg",
+        'title': "fσllσw чσutuвє chαnnєl📄",
+        'вσdч': "clíck tσ suвscríвє thє σffícíαl njαвulσ jв чσutuвє chαnnєl!"
+      }
+    }
+  });
+  console.log("Command executed: ytchannel");
+});
+zokou({
+  'nomCom': 'repoxmd',
+  'reaction': '🪰',
+  'categorie': "Njabulo-Jb",
+  'nomFichier': __filename
+}, async (_0x14c950, _0x346e6b, _0x31cbea) => {
+  console.log("Commande saisie !!!s");
+  await _0x346e6b.sendMessage(_0x14c950, {
+    'text': "Hello 👋\n\nclíck σn thє phσtσ вєlσw tσ cσnnєct thє σffícíαl *njαвulσ jв* tєlєgrαm ínвσх",
+    'contextInfo': {
+      'externalAdReply': {
+        'sourceUrl': 'https.github.com/Njabulo-Jb',
+        'mediaType': 0x1,
+        'mediaUrl': "https://files.catbox.moe/hlazmb.jpg",
+        'title': "Chating With Owner",
+        'body': "clíck tσ cσntαct thє σffícíαl njαвulσ jв tєlєgrαm ínвσх!"
+      }
+    }
+  });
+  console.log("Command executed: t-help");
+});
