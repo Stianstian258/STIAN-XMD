0633258a4e63c679f05f4297bd5bd41d24b21038
NjabuloJ <fanajb65@gmail.com>
2025-03-23 00:19:33 +0200

    Update menu (1).js

--- commandes/menu (1).js
+++ commandes/menu (1).js
@@ -28,9 +28,9 @@ zokou({ nomCom: "list", categorie: "General" }, async (dest, zk, commandeOptions
     const date = moment().format('DD/MM/YYYY');
 
     let infoMsg = `
-   ☻ •Ꮹσσd єvєníng : ${nomAuteurMessage}
+☻ •Ꮹσσd єvєníng : ${nomAuteurMessage}
 
-╭━•••••⪼
+╭━•••••⪼ 
 ┋ ☻ σwnєr : hαppínєss
 ┋ ☻ prєfíх : [ ${s.PREFIXE} ]
 ┋ ☻ mσdє : ${mode}
@@ -38,20 +38,335 @@ zokou({ nomCom: "list", categorie: "General" }, async (dest, zk, commandeOptions
 ┋ ☻ plαtfσrm : ${os.platform()}
 ┋ ☻ σwnєr : ɴᴊᴀʙᴜʟᴏ ᴊʙ 
 ┋ ☻ plugíns : ${cm.length}
-╰━•••••⪼ \n`;
+╰━•••••⪼
 
-    let menuMsg = ` ${readmore}`;
-
-    for (const category in commandsList) {
-        menuMsg += `
- `${category}`
-╭━•••••⪼ `;
-        for (const cmd of commandsList[category]) {
-            menuMsg += `          
-┋ ☻ •   ${cmd}`;
-        }
-        menuMsg += `
-╰━•••••⪼`;
+    let menuMsg = ` ${readmore}
+⪼íα
+╭─•••••⪼ 
+┋ ☻ • njαвulσ 
+┋ ☻ • dαllє 
+┋ ☻ • αí 
+┋ ☻ • gpt
+╰─•••••⪼
+⪼gєnєrαl
+╭─•••••⪼  
+┋ ☻ • σwnєr 
+┋ ☻ • dєv 
+┋ ☻ • suppσrt 
+┋ ☻ • αntí-dєlєtє 
+┋ ☻ • αntí-stíckєr-dєlєtє 
+┋ ☻ • вíвlє 
+┋ ☻ • pσll 
+┋ ☻ • вrσαdcαst 
+┋ ☻ • cσntαctsαvє 
+┋ ☻ • gєtαllmєmвєrs 
+┋ ☻ • chαnnєll 
+┋ ☻ • grσups 
+┋ ☻ • updαtє 
+┋ ☻ • nαmєвσt 
+┋ ☻ • nαmєσwnєr 
+┋ ☻ • hαck 
+┋ ☻ • undєfínєd 
+┋ ☻ • mєnu 
+┋ ☻ • σвt 
+┋ ☻ • tєst 
+┋ ☻ • uptímє 
+┋ ☻ • pαír 
+┋ ☻ • píng 
+┋ ☻ • αrєαct 
+┋ ☻ • ímαg-gєnєrαtє 
+┋ ☻ • tσєхtrαct 
+┋ ☻ • fluх-ímg 
+┋ ☻ • mínα 
+┋ ☻ • dвєαutífч
+╰─•••••⪼
+⪼dσwnlσαd
+╭─•••••⪼ 
+┋ ☻ • vídєσ1 
+┋ ☻ • αpk 
+┋ ☻ • ínstαgrαm 
+┋ ☻ • fαcєвσσk 
+┋ ☻ • tíktσk 
+┋ ☻ • lítє 
+┋ ☻ • plαч1
+╰─•••••⪼
+⪼fun
+╭─•••••⪼ 
+┋ ☻ • rαnímє 
+┋ ☻ • fαncч 
+┋ ☻ • rαnk 
+┋ ☻ • tσprαnk
+╰─•••••⪼
+⪼sєαrch
+╭─•••••⪼
+┋ ☻ • hєlp  
+┋ ☻ • gσσglє 
+┋ ☻ • ímdв 
+┋ ☻ • mσvíє 
+┋ ☻ • dєfínє 
+┋ ☻ • lчrícs 
+┋ ☻ • ímg 
+┋ ☻ • gαlαхч 
+┋ ☻ • plαч 
+┋ ☻ • vídєσ3 
+┋ ☻ • íphσnє12prσ
+╰─•••••⪼
+⪼cσnvєrsíσn
+╭─•••••⪼  
+┋ ☻ • єmσmíх 
+┋ ☻ • stíckєr 
+┋ ☻ • scrσp 
+┋ ☻ • tαkє 
+┋ ☻ • wrítє 
+┋ ☻ • phσtσ 
+┋ ☻ • trt 
+┋ ☻ • url 
+┋ ☻ • tєstαmєnt
+╰─•••••⪼
+⪼usєr
+╭─•••••⪼  
+┋ ☻ • fαct 
+┋ ☻ • quσtєs 
+┋ ☻ • dít 
+┋ ☻ • íttα 
+┋ ☻ • sαч
+╰─•••••⪼
+⪼ímαgє-єdít
+╭─•••••⪼  
+┋ ☻ • shít 
+┋ ☻ • wαstєd 
+┋ ☻ • wαntєd 
+┋ ☻ • tríggєr 
+┋ ☻ • trαsh 
+┋ ☻ • ríp 
+┋ ☻ • sєpíα 
+┋ ☻ • rαínвσw 
+┋ ☻ • hítlєr 
+┋ ☻ • ínvєrt 
+┋ ☻ • jαíl 
+┋ ☻ • αffєct 
+┋ ☻ • вєαutíful 
+┋ ☻ • вlur 
+┋ ☻ • círclє 
+┋ ☻ • fαcєpαlm 
+┋ ☻ • grєчscαlє 
+┋ ☻ • jσkє
+╰─•••••⪼
+⪼gαmєs
+╭─•••••⪼  
+┋ ☻ • ríddlє 
+┋ ☻ • chífumí 
+┋ ☻ • quízz
+╰─•••••⪼
+⪼grσup
+╭─•••••⪼  
+┋ ☻ • αdd 
+┋ ☻ • dísαp-σff 
+┋ ☻ • dísαp 
+┋ ☻ • rєq 
+┋ ☻ • dísαp90 
+┋ ☻ • rєjєct 
+┋ ☻ • dísαp7 
+┋ ☻ • dísαp1 
+┋ ☻ • αpprσvє 
+┋ ☻ • vcf 
+┋ ☻ • ínvítє 
+┋ ☻ • rєvσkє 
+┋ ☻ • αntíwσrd 
+┋ ☻ • αntílínk-αll 
+┋ ☻ • tαgαll 
+┋ ☻ • línk 
+┋ ☻ • prσmσtє 
+┋ ☻ • dєmσtє 
+┋ ☻ • rєmσvє 
+┋ ☻ • dєl 
+┋ ☻ • ínfσ 
+┋ ☻ • αntílínk 
+┋ ☻ • αntíвσt 
+┋ ☻ • grσup 
+┋ ☻ • gnαmє 
+┋ ☻ • gdєsc 
+┋ ☻ • gpp 
+┋ ☻ • hídєtαg 
+┋ ☻ • αutσmutє 
+┋ ☻ • αutσunmutє 
+┋ ☻ • fkíck 
+┋ ☻ • nsfw 
+┋ ☻ • kíckαll 
+┋ ☻ • σnlчαdmín 
+┋ ☻ • tαgαdmín 
+┋ ☻ • wαrn 
+┋ ☻ • wαrn
+╰─•••••⪼
+⪼mσds
+╭─•••••⪼
+┋ ☻ • lєft 
+┋ ☻ • tєlєstíckєr 
+┋ ☻ • crєw 
+┋ ☻ • lєft 
+┋ ☻ • jσín 
+┋ ☻ • jíd 
+┋ ☻ • вlσck 
+┋ ☻ • unвlσck 
+┋ ☻ • вαn 
+┋ ☻ • вαngrσup 
+┋ ☻ • sudσ 
+┋ ☻ • sαvє 
+┋ ☻ • mєntíσn 
+┋ ☻ • lulcαtч 
+┋ ☻ • sαdcαtч 
+┋ ☻ • nσkíαh 
+┋ ☻ • unfσrgívαв 
+┋ ☻ • pσσhh 
+┋ ☻ • σhσgwαч 
+┋ ☻ • jвídєn 
+┋ ☻ • hdríp 
+┋ ☻ • clσwns 
+┋ ☻ • tєrmínαt
+╰─•••••⪼
+⪼hєntαí
+╭─•••••⪼  
+┋ ☻ • hwαífu 
+┋ ☻ • trαp 
+┋ ☻ • hnєkσ 
+┋ ☻ • вlσwjσв 
+┋ ☻ • hєntαívíd 
+┋ ☻ • hwαífu 
+┋ ☻ • trαp 
+┋ ☻ • hnєkσ 
+┋ ☻ • вlσwjσв 
+┋ ☻ • hєntαívíd 
+┋ ☻ • αss 
+┋ ☻ • mαstєrвαtíσn 
+┋ ☻ • thígh 
+┋ ☻ • pαntч 
+┋ ☻ • wαífuєs1 
+┋ ☻ • trαp1 
+┋ ☻ • nєkσ1 
+┋ ☻ • вlσwjσв1
+╰─•••••⪼
+⪼hєrσku
+╭─•••••⪼  
+┋ ☻ • sєtvαr 
+┋ ☻ • αllvαr 
+┋ ☻ • gєtvαr
+╰─•••••⪼
+⪼lσgσ
+╭─•••••⪼  
+┋ ☻ • hαckєr 
+┋ ☻ • drαgσnвαll 
+┋ ☻ • nαrutσ 
+┋ ☻ • dídσng 
+┋ ☻ • wαll 
+┋ ☻ • summєr 
+┋ ☻ • nєσnlíght 
+┋ ☻ • grєєnnєσn 
+┋ ☻ • glítch 
+┋ ☻ • dєvíl 
+┋ ☻ • вσσm 
+┋ ☻ • wαtєr 
+┋ ☻ • snσw 
+┋ ☻ • trαnsfσrmєr 
+┋ ☻ • thundєr 
+┋ ☻ • hαrrчpσttєr 
+┋ ☻ • cαt 
+┋ ☻ • whítєgσld 
+┋ ☻ • líghtglσw 
+┋ ☻ • thσr 
+┋ ☻ • nєσn 
+┋ ☻ • purplє 
+┋ ☻ • gσld 
+┋ ☻ • αrєnα 
+┋ ☻ • íncαndєscєnt
+╰─•••••⪼
+⪼sєαrch
+╭─•••••⪼
+┋ ☻ • lчrícs 
+┋ ☻ • scrєєnswídth 
+┋ ☻ • scrєєnscrσp 
+┋ ☻ • mαхαgє 
+┋ ☻ • jpg 
+┋ ☻ • png 
+┋ ☻ • nσαnímαtє 
+┋ ☻ • wαít 
+┋ ☻ • víєwpσrtwídth 
+┋ ☻ • íphσnє5 
+┋ ☻ • íphσnє6 
+┋ ☻ • íphσnє6plus 
+┋ ☻ • íphσnєх 
+┋ ☻ • íphσnє14prσmαх 
+┋ ☻ • gαlαхчs5 
+┋ ☻ • scrєєnshσt 
+┋ ☻ • ímgs 
+┋ ☻ • mєssí
+╰─•••••⪼
+⪼rєαctíσn
+╭─•••••⪼  
+┋ ☻ • вullч 
+┋ ☻ • cuddlє 
+┋ ☻ • crч 
+┋ ☻ • hug 
+┋ ☻ • αwσσ 
+┋ ☻ • kíss 
+┋ ☻ • líck 
+┋ ☻ • pαt 
+┋ ☻ • smug 
+┋ ☻ • вσnk 
+┋ ☻ • чєєt 
+┋ ☻ • вlush 
+┋ ☻ • smílє 
+┋ ☻ • wαvє 
+┋ ☻ • híghfívє 
+┋ ☻ • hαndhσld 
+┋ ☻ • nσm 
+┋ ☻ • вítє 
+┋ ☻ • glσmp 
+┋ ☻ • slαp 
+┋ ☻ • kíll 
+┋ ☻ • kíck 
+┋ ☻ • hαppч 
+┋ ☻ • wínk 
+┋ ☻ • pσkє 
+┋ ☻ • dαncє 
+┋ ☻ • críngє
+╰─•••••⪼
+⪼gíthuв
+╭─•••••⪼ 
+┋ ☻ • gít 
+┋ ☻ • rєpσ 
+┋ ☻ • scrípt 
+┋ ☻ • sc
+╰─•••••⪼
+⪼hєrσku-clíєnt
+╭─•••••⪼  
+┋ ☻ • αntícαll 
+┋ ☻ • rєαdstαtus 
+┋ ☻ • αntídєlєtє 
+┋ ☻ • dσwnlσαdstαtus 
+┋ ☻ • stαrtmєssαgє 
+┋ ☻ • rєαdmєssαgє 
+┋ ☻ • pm-pєrmít 
+┋ ☻ • chαtвσt 
+┋ ☻ • grєєt 
+┋ ☻ • αntívv 
+┋ ☻ • puвlícmσdє 
+┋ ☻ • αutσrєcσrd 
+┋ ☻ • αutσtчpíng 
+┋ ☻ • αlwαчsσnlínє 
+┋ ☻ • prívαtєmσdє 
+┋ ☻ • αutσlíkєstαtus 
+┋ ☻ • chαtвσt 
+┋ ☻ • sєttíngs
+╰─•••••⪼
+⪼wєєв
+╭─•••••⪼  
+┋ ☻ • wαífu 
+┋ ☻ • nєkσ 
+┋ ☻ • shínσвu 
+┋ ☻ • mєgumín 
+┋ ☻ • cσsplαч 
+┋ ☻ • cσuplєpp
+╰─•••••⪼`;
     }
 
     menuMsg += `\n> sir Njabulo Jb 2025`;
