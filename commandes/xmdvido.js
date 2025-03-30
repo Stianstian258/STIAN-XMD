d6bb27c257915509bc6e5d312d23ace70669ab0b
NjabuloJ <fanajb65@gmail.com>
2025-03-31 00:36:18 +0200

    Update xmdvido.js

--- commandes/xmdvido.js
+++ commandes/xmdvido.js
@@ -68,14 +68,14 @@ zokou({
     // Prepare the message payload with external ad details
     const messagePayloads = [
       {
-       caption: `\n================================\nNJABULO JB DOWNLOAD\n
+       caption: `\n================================\n*NJABULO JB DOWNLOAD*\n
 ================================
- *Title:* ${videoDetails.title} 
- *Quality:* High
- *Duration:* ${firstVideo.timestamp}
+ Title: ${videoDetails.title} 
+ Quality: High
+ Duration: ${firstVideo.timestamp}
 ================================
 
-> sir Njabulo Jb 
+> *sir Njabulo Jb* 
 `,
         document: { url: downloadUrl },
         mimetype: 'video/mp4',
