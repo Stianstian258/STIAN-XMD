b69fefe7e2d6f9d7e5aee16b1d399f1f9d0c1e4a
NjabuloJ <fanajb65@gmail.com>
2025-03-31 12:47:49 +0200

    Update ping1.js

--- commandes/ping1.js
+++ commandes/ping1.js
@@ -1,29 +1,36 @@
-const util = require('util');
-const fs = require('fs-extra');
-const axios = require('axios');
-const { zokou } = require(__dirname + "/../framework/zokou");
-const os = require("os");
+const { zokou } = require("../framework/zokou");
 const moment = require("moment-timezone");
-const set = require(__dirname + "/../set");
+const { getBuffer } = require("../framework/dl/Function");
+const { default: axios } = require('axios');
 
 const AUDIO_URL = "https://files.catbox.moe/e52xx6.mp3"; // New audio URL
 const THUMBNAIL_URL = "https://files.catbox.moe/533oqh.jpg"; // New image URL
 
-moment.tz.setDefault(`${set.TZ}`);
+const runtime = function (seconds) { 
+ seconds = Number(seconds); 
+ var d = Math.floor(seconds / (3600 * 24)); 
+ var h = Math.floor((seconds % (3600 * 24)) / 3600); 
+ var m = Math.floor((seconds % 3600) / 60); 
+ var s = Math.floor(seconds % 60); 
+ var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " d, ") : ""; 
+ var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " h, ") : ""; 
+ var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " m, ") : ""; 
+ var sDisplay = s > 0 ? s + (s == 1 ? " second" : " s") : ""; 
+ return dDisplay + hDisplay + mDisplay + sDisplay; 
+ } 
 
-const getTimeAndDate = () => {
-    return {
-        time: moment().format('HH:mm:ss'),
-        date: moment().format('DD/MM/YYYY')
-    };
-};
 
-// Ping Command
-zokou({ nomCom: "uptime", categorie: "General" }, async (dest, zk, commandeOptions) => {
-    let { ms } = commandeOptions;
-    const { time, date } = getTimeAndDate();
-    const ping = Math.floor(Math.random() * 100) + 1; // Generate a random ping between 1ms - 100ms
+zokou({ nomCom: 'uptime',
+    desc: 'To check runtime',
+    Categorie: 'General',
+    reaction: '⚙️', 
+    fromMe: 'true', 
 
+
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+      
     try {
         await zk.sendMessage(dest, { 
         audio: { url: AUDIO_URL }, 
@@ -49,7 +56,7 @@ zokou({ nomCom: "uptime", categorie: "General" }, async (dest, zk, commandeOptio
         }, { quoted: ms });
 
     } catch (e) {
-        console.log("❌ Ping Command Error: " + e);
+        console.log("❌ uptime Command Error: " + e);
         repondre("❌ Error: " + e);
     }
 });
