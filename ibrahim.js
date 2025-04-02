8f38144c70dd09f94f2744fa2e6491469a0f8368
Marisel <shenapam3@gmail.com>
2025-04-02 16:13:11 +0300

    Update ibrahim.js

--- ibrahim.js
+++ ibrahim.js
@@ -1,3 +1,5 @@
+//COPPY CODER ALLERT HEY CLONNER HOPE YOU DONT WANT TO COPY THIS CODE.
+
 "use strict";
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
