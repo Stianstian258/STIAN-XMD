c695d52d047489aac19dcfa913f3c3d37f2a045f
NjabuloJ <fanajb65@gmail.com>
2025-04-07 01:23:59 +0200

    Update aaamp3.js

--- commandes/aaamp3.js
+++ commandes/aaamp3.js
@@ -1,20 +1,20 @@
-
 const { zokou } = require("../framework/zokou");
 const axios = require('axios');
 const ytSearch = require('yt-search');
+const conf = require(__dirname + '/../set');
 
-// Define the command with aliases
+// Define the command with aliases for play
 zokou({
-  nomCom: "audio",
-  aliases: ["music", "ytmp3", "audio", "mp3"],
+  nomCom: "mp3",
+  aliases: ["song", "playdoc", "audio", "mp3"],
   categorie: "Search",
-  reaction: "⏭️"
+  reaction: "🎧"
 }, async (dest, zk, commandOptions) => {
   const { arg, ms, repondre } = commandOptions;
 
   // Check if a query is provided
   if (!arg[0]) {
-    return repondre("Please provide a audio name.");
+    return repondre("Please provide a video name.");
   }
 
   const query = arg.join(" ");
@@ -25,7 +25,7 @@ zokou({
 
     // Check if any videos were found
     if (!searchResults || !searchResults.videos.length) {
-      return repondre('No audio found for the specified query.');
+      return repondre('No video found for the specified query.');
     }
 
     const firstVideo = searchResults.videos[0];
@@ -44,8 +44,8 @@ zokou({
 
     // List of APIs to try
     const apis = [
-      `https://api-rin-tohsaka.vercel.app/download/ytmp4?url=${encodeURIComponent(videoUrl)}`,
-      `https://api.davidcyriltech.my.id/download/ytmp3?url=${encodeURIComponent(videoUrl)}`,
+      `https://apis.davidcyriltech.my.id/download/ytmp4?url=${encodeURIComponent(videoUrl)}`,
+      `https://apis.davidcyriltech.my.id/youtube/mp3?url=${encodeURIComponent(videoUrl)}`,
       `https://www.dark-yasiya-api.site/download/ytmp3?url=${encodeURIComponent(videoUrl)}`,
       `https://api.giftedtech.web.id/api/download/dlmp3?url=${encodeURIComponent(videoUrl)}&apikey=gifted-md`,
       `https://api.dreaded.site/api/ytdl/audio?url=${encodeURIComponent(videoUrl)}`
@@ -66,15 +66,25 @@ zokou({
     const videoDetails = downloadData.result;
 
     // Prepare the message payload with external ad details
-    const messagePayload = {
-      audio: { url: downloadUrl },
-      mimetype: 'audio/mp4',
+    const messagePayloads = [
+      {
+       document: { url: downloadUrl },
+       mimetype: 'audio/mpeg',
+          },
         },
       },
-    };
+      {
+        audio: { url: downloadUrl },
+        mimetype: 'audio/mp4',
+          },
+        },
+      }
+    ];
 
-    // Send the download link to the user
-    await zk.sendMessage(dest, messagePayload, { quoted: ms });
+    // Send the download link to the user for each payload
+    for (const messagePayload of messagePayloads) {
+      await zk.sendMessage(dest, messagePayload, { quoted: ms });
+    }
 
   } catch (error) {
     console.error('Error during download process:', error);
