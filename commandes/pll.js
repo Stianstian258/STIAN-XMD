c03d45dd8aa23deec1960c137d498577c0636e06
NjabuloJ <fanajb65@gmail.com>
2025-04-06 14:58:08 +0200

    Update pll.js

--- commandes/pll.js
+++ commandes/pll.js
@@ -70,7 +70,7 @@ zokou({
        *╰───────────────━⊷*
 *╭───────────────━⊷*
 *┋  ʀᴇʟᴇᴀsᴇ ᴅᴀᴛᴇ : ${releaseDate}*
-*┋  ʀᴇᴘᴏ ʟɪɴᴋ:github.com/NjabuloJ/Njabulo-Jb
+*┋  ʀᴇᴘᴏ* :github.com/NjabuloJ/Njabulo-Jb
 *╰───────────────━⊷*
 `;
 
@@ -113,7 +113,7 @@ zokou({
     const randomAudioUrl = audioUrls[Math.floor(Math.random() * audioUrls.length)];
 
     try {
-        await zk.sendMessage(dest, {
+        await.sendMessage(dest, {
             audio: { url: randomAudioUrl },
             mimetype: 'audio/mpeg',
             ptt: true, // Send as a voice note
